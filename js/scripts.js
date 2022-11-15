9eef1032386cc24b7ac90736e0e6a37b3e53c164
Charles Cookman <CharlesBCookman@gmail.com>
2022-11-14 16:51:36 -0800

    Moved questions to answer tree. Fixed spelling and formatting issues throughout.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,3 @@
-function questionChangeTo(textHere) {
-  let questionsText = document.getElementById("questions-text").innertext;
-  questionsText = textHere;
-}
-
-function changeRadioOne(answerOne) {
-  const radioOne = document.getElementById("label1");
-  radioOne.checked = false;
-  let radioOneText = document.getElementById("label1").innerText;
-  radioOneText = answerOne;
-}
-
-function changeRadioTwo(answerTwo) {
-  const radioTwo = document.getElementById("label2");
-  radioTwo.checked = false;
-  let radioTwoText = document.getElementById("label2").innerText;
-  radioTwoText = answerTwo;
-}
-
-function changeRadioThree(answerThree) {
-  const radioThree = document.getElementById("label3");
-  radioThree.checked = false;
-  let radioThreeText = document.getElementById("label3").innerText;
-  radioThreeText = answerThree;
-}
-
 function removeHiddenOne(event) {
   event.preventDefault();
   console.log("is this working")
@@ -52,177 +26,177 @@ function showAnswer(event) {
   const namePlace = document.getElementById("showname");
 }
 
-function questionTwo(event) {
-  event.preventDefault();
-  removeHiddenTwo
-  document.getElementById("radio1").value = "q2a1";
-  document.getElementById("radio2").value = "q2a2";
-  documen.getElementById("radio3").value = "q2a3";
-  questionChangeTo("Which do you prefer:");
-  changeRadioOne("Problem solving and/or logic problems.");
-  changeRadioTwo("Beautiful things or things that run smooth.");
-  changeRadioThree("Complex things that require thought and analysis.");
-}
-
-function questionThree(event) {
-  event.preventDefault();
-  removeHiddenTwo
-  document.getElementById("radio1").value = "q3a1";
-  document.getElementById("radio2").value = "q3a2";
-  documen.getElementById("radio3").value = "q3a3";
-  questionChangeTo("Which do you prefer:");
-  changeRadioOne("Word/Logic problems/puzzles.");
-  changeRadioTwo("Writing and reading beautiful literature.");
-  changeRadioThree("Analyzing, and/or editing.");
-}
-
-function questionFour(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q4a1";
-  document.getElementById("radio2").value = "q4a2";
-  documen.getElementById("radio3").value = "q4a3";
-  questionChangeTo("When you problem solve, do you…");
-  changeRadioOne("Like a big challenge.");
-  changeRadioTwo("Enjoy thinking through your options.");
-  changeRadioThree("Do it quickly so you can move on to the next one.");
-}
-
-function questionFive(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q5a1";
-  document.getElementById("radio2").value = "q5a2";
-  documen.getElementById("radio3").value = "q5a3";
-  questionChangeTo("Do you like…");
-  changeRadioOne("Complex work with contradictions.");
-  changeRadioTwo("Approachable work without ambiguity.");
-  changeRadioThree("Fast paced work with plenty reference material.");
-}
-
-function questionSix(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q6a1";
-  document.getElementById("radio2").value = "q6a2";
-  documen.getElementById("radio3").value = "q6a3";
-  questionChangeTo("Would you rather…");
-  changeRadioOne("Complex work with contradictions.");
-  changeRadioTwo("Approachable work without ambiguity.");
-  changeRadioThree("Fast paced work with plenty reference material.");
-}
-
-function questionSeven(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q7a1";
-  document.getElementById("radio2").value = "q7a2";
-  documen.getElementById("radio3").value = "q7a3";
-  questionChangeTo("Do you prefer Mac or PC’s");
-  changeRadioOne("Mac");
-  changeRadioTwo("PC’s");
-  changeRadioThree("Both");
-}
-
-function questionEight(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q8a1";
-  document.getElementById("radio2").value = "q8a2";
-  documen.getElementById("radio3").value = "q8a3";
-  questionChangeTo("Do you prefer tools that…");
-  changeRadioOne("Work really easily and quickly.");
-  changeRadioTwo("Do a lot of things.");
-  changeRadioThree("Are really good at what they do.");
-}
-
-function questionNine(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q9a1";
-  document.getElementById("radio2").value = "q9a2";
-  documen.getElementById("radio3").value = "q9a3";
-  questionChangeTo("Are you a...");
-  changeRadioOne("Fast paced get it done kinda person.");
-  changeRadioTwo("I like a good challenge I can spend time on.");
-  changeRadioThree("Bring it on kinda person.");
-}
-
-function questionTen(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q10a1";
-  document.getElementById("radio2").value = "q10a2";
-  documen.getElementById("radio3").value = "q10a3";
-  questionChangeTo("Would you rather work on…");
-  changeRadioOne("Servers.");
-  changeRadioTwo("Functionality and data analytics kinda stuff.");
-  changeRadioThree("A mix of everything.");
-}
-
-function questionEleven(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q11a1";
-  document.getElementById("radio2").value = "q11a2";
-  documen.getElementById("radio3").value = "q11a3";
-  questionChangeTo("Would you rather work on…");
-  changeRadioOne("Servers or windows software.");
-  changeRadioTwo("Functionality and data analytics kinda stuff.");
-  changeRadioThree("A mix of everything.");
-}
-
-function questionTwelve(event) {
-  event.preventDefault();
-  document.getElementById("radio1").value = "q12a1";
-  document.getElementById("radio2").value = "q12a2";
-  documen.getElementById("radio3").value = "q12a3";
-  questionChangeTo("Would you rather work on…");
-  changeRadioOne("Servers or windows software.");
-  changeRadioTwo("Work with servers and build back end functionality.");
-  changeRadioThree("A lot of everything.");
-}
 
 function finalAnswers(event) {
   event.preventDefault();
 }
 
-function checkAnswer(answer) {
-  const radioOne = document.getElementById("radio1");
-  const radioTwo = document.getElementById("radio2");
-  const radioThree = document.getElementById("radio3");
-  if(radioOne.checked = true) {
-    answer = radioOne.value;
-  }
-  if(radioTwo.checked = true) {
-    answer = radioTwo.value;
-  }
-  if(radioThree.checked = true) {
-    answer = radioThree.value;
-  }
-  else {
-    alert("broken")
-  }
-  console.log(answer)
-}
-
 function answersTree(event) {
   event.preventDefault();
-  checkAnswer(answer);
+  let answer;
+  answer = document.querySelector("input[name='radio']:checked").value;
+  console.log(answer)
+  const questionText = document.getElementById("questions-text").innerText;
+  const radioOne = document.getElementById("radio1").value;
+  const radioTwo = document.getElementById("radio2").value;
+  const radioThree = documen.getElementById("radio3").value;
+  const labelOne = getElementById("label1").innertext;
+  const labelTwo = getElementById("label2").inntertext;
+  const labelThree = getElementById("label3").inntertext;
+
+  function questionTwo() {
+    //question2
+    radioOne = "q2a1"
+    radioTwo = "q2a2"
+    radioThree = "q2a3"
+    questionText = "2. Which do you prefer:"
+    labelOne = "Problem solving and/or logic problems."
+    labelTwo = "Beautiful things or things that run smooth?"
+    labelThree = "Complex things that require thought and analysis?"
+  }
+  function questionThree() {
+    //questio3
+    radioOne = "q3a1";
+    radioTwo = "q3a2";
+    radioThree = "q3a3";
+    questionText = "Which do you prefer:";
+    labelOne = "Word/Logic problems/puzzles.";
+    labelTwo = "Writing and reading beautiful literature.";
+    labelThree = "Analyzing, and/or editing.";
+}
+
+  function questionFour() {
+    //question4
+    radioOne = "q4a1";
+    radioTwo = "q4a2";
+    radioThree = "q4a3";
+    questionText = "When you problem solve, do you…";
+    labelOne = "Like a big challenge.";
+    labelTwo = "Enjoy thinking through your options.";
+    labelThree = "Do it quickly so you can move on to the next one.";
+  }
+
+  function questionFive() {
+    //question5
+    radioOne = "q5a1";
+    radioTwo = "q5a2";
+    radioThree = "q5a3";
+    questionText = "Do you like…";;
+    labelOne = "Complex work with contradictions.";
+    labelTwo = "Approachable work without ambiguity.";
+    labelThree = "Fast paced work with plenty reference material.";
+  }
+
+  function questionSix() {
+    //question6
+    radioOne = "q6a1";
+    radioTwo = "q6a2";
+    radioThree = "q6a3";
+    questionText = "Would you rather…";
+    labelOne = "Learn software stuff.";
+    labelTwo = "Learn internet stuff.	";
+    labelThree = "Learn a lot of stuff.";
+  }
+
+  function questionSeven() {
+    //question7
+    radioOne = "q7a1";
+    radioTwo = "q7a2";
+    radioThree = "q7a3";
+    questionText = "Do you prefer Mac or PC’s";
+    labelOne = "Mac";
+    labelTwo = "PC’s";
+    labelThree = "Both";
+  }
+
+  function questionEight() {
+    //question8
+    radioOne = "q8a1";
+    radioTwo = "q8a2";
+    radioThree = "q8a3";
+    questionText = "Do you prefer tools that…";
+    labelOne = "Work really easily and quickly.";
+    labelTwo = "Do a lot of things.";
+    labelThree = "Are really good at what they do.";
+  }
+
+  function questionNine() {
+    //question9
+    radioOne = "q9a1";
+    radioTwo = "q9a2";
+    radioThree = "q9a3";
+    questionText = "Are you a...";
+    labelOne = "Fast paced get it done kinda person.";
+    labelTwo = "I like a good challenge I can spend some time on.";
+    labelThree = "What ever it is bring it on kinda person.";
+  }
+  
+  function questionTen() {
+    //question10
+    radioOne = "q10a1";
+    radioTwo = "q10a2";
+    radioThree = "q10a3";
+    questionText = "Would you rather work on…";
+    labelOne = "Servers.";
+    labelTwo = "Functionality and data analytics kinda stuff.";
+    labelThree = "A mix of everything.";
+  }
+
+  function questionEleven() {
+    //question11
+    radioOne = "q11a1";
+    radioTwo = "q11a2";
+    radioThree = "q11a3";
+    questionText = "Would you rather work on…";
+    labelOne = "Servers or windows software.";
+    labelTwo = "Functionality and data analytics kinda stuff.";
+    labelThree = "A mix of everything.";
+  }
+
+  function questionTwelve() {
+    //question12
+    radioOne = "q12a1";
+    radioTwo = "q12a2";
+    radioThree = "q12a3";
+    questionText = "Windows servers and software.";
+    labelOne = "Servers or windows software.";
+    labelTwo = "Work with servers and build back end functionality.";
+    labelThree = "A lot of everything.";
+  }
+
   if(answer = "q1a1"){
+      //question2
+    removeHiddenTwo;
     questionTwo;
   }
   if(answer = "q1a2"){
+    //question3
+    removeHiddenTwo;
     questionThree;
+
+    
   }
   if(answer = "q2a1"){
-    questionFour
+    questionFour;
+
   }
   if(answer = "q2a2"){
-    questionFive;
+
   }
   if(answer = "q2a3"){
+    //question6
     questionSix;
   }
   if(answer = "q3a1"){
+    //question7
     questionFour;
   }
   if(answer = "q3a2"){
+    //question8
     questionFive;
   }
   if(answer = "q3a3"){
+    //question9
     questionSix;
   }
   if(answer = "q4a1"){
@@ -310,9 +284,11 @@ function answersTree(event) {
     alert("Something went wrong...")
 } }
 
+
+
 window.addEventListener("load", function(){
   const startButton = document.getElementById("start");
   startButton.addEventListener("click", removeHiddenOne);
   const nextButton = document.getElementById("next-button");
-  nextButton.addEventListener("click", removeHiddenTwo, answersTree);
+  nextButton.addEventListener("click", answersTree);
 });
